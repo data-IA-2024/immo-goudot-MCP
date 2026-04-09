094deb7566e6f48603c3f0ec203245c5bd2714c5
goudot <Emmanuel.Goudot@gmail.com>
2026-04-09 13:42:28 +0200

    UPD client

--- client.py
+++ client.py
@@ -1,11 +1,19 @@
 import asyncio
 from fastmcp import Client
+from dotenv import load_dotenv  # pip install python-dotenv
+import os, time
 
-client = Client("http://localhost:8000/mcp")
+load_dotenv()
+
+MCP_SERVER=os.getenv("MCP_SERVER")
+client = Client(MCP_SERVER)
+print(f"{MCP_SERVER=}")
 
 async def call_tool(name: str):
     async with client:
         result = await client.call_tool("greet", {"name": name})
         print(result)
 
-asyncio.run(call_tool("Emmanuel"))
+while True:
+    asyncio.run(call_tool("Emmanuel"))
+    time.sleep(30)
